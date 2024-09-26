f236b3e6fbe0ff52bd14743c4cf4b3f22427a79d
yashtekade21 <128163324+yashtekade21@users.noreply.github.com>
2024-09-26 12:13:32 +0530

    Create 1_Two Sum.cpp

---
+++ 1_Two Sum.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        for (int i = 0; i < nums.size() - 1; i++) {
+            for (int j = i + 1; j < nums.size(); j++) {
+                if (nums[i] + nums[j] == target) {
+                    return {i, j};
+                }
+            }
+        }
+        return {};
+    }
+};
+
+static const auto kds = []() {
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+    std::cout.tie(nullptr);
+    return 0;
+}();
