6e080b46fc5eb8f833218c4718d611ac9b8ea572
yashtekade21 <128163324+yashtekade21@users.noreply.github.com>
2024-10-12 13:38:22 +0530

    Create 88_Merge_Sorted_Array.cpp

---
+++ 88_Merge_Sorted_Array.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+        int i = m - 1, j = n - 1;
+        int k = m + n - 1;
+
+        while (j >= 0) {
+            if (i >= 0 && nums1[i] > nums2[j]) {
+                nums1[k] = nums1[i];
+                i--;
+            } else {
+                nums1[k] = nums2[j];
+                j--;
+            }
+            k--;
+        }
+
+        return;
+    }
+};
+static const auto kds = []() {
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+    std::cout.tie(nullptr);
+    return 0;
+}();
