ae1e341ed285acc77935b7126fbcf29341e90741
Yash Tekade <128163324+yashtekade21@users.noreply.github.com>
2024-10-16 20:14:05 +0530

    Create 33_Search_in_Rotated_Sorted_Array.cpp

---
+++ 33_Search_in_Rotated_Sorted_Array.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int n = nums.size();
+        int low = 0, high = n - 1;
+        while (low <= high) {
+            int mid = (low + high) / 2;
+
+            if (nums[mid] == target)
+                return mid;
+
+            if (nums[low] <= nums[mid]) {
+                if (nums[low] <= target && nums[mid] >= target)
+                    high = mid - 1;
+                else
+                    low = mid + 1;
+            } else {
+                if (nums[mid] <= target && nums[high] >= target)
+                    low = mid + 1;
+                else
+                    high = mid - 1;
+            }
+        }
+        return -1;
+    }
+};
+static const auto kds = []() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return 0;
+}();
