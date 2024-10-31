3a1a902f71bd1f0d03e8e67ac1cf54c12421a31e
Yash Tekade <128163324+yashtekade21@users.noreply.github.com>
2024-10-31 19:37:40 +0530

    Create 104_Maximum_Depth_of_Binary_Tree.cpp

---
+++ 104_Maximum_Depth_of_Binary_Tree.cpp
@@ -0,0 +1,29 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int maxDepth(TreeNode* root) {
+        if(root == nullptr)
+        return 0;
+
+        int left = maxDepth(root->left);
+        int right = maxDepth(root->right);
+
+        return max(left,right)+1;
+    }
+};
+static const auto kds = []() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return 0;
+}();
