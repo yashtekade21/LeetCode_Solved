7c4dfee8b329b82ba81925cc3fde792678411610
Yash Tekade <128163324+yashtekade21@users.noreply.github.com>
2024-12-11 18:12:36 +0530

    Create 141_Linked_List_Cycle.cpp

---
+++ 141_Linked_List_Cycle.cpp
@@ -0,0 +1,29 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    bool hasCycle(ListNode *head) {
+        unordered_map<ListNode*,int> mp;
+
+        ListNode* temp = head;
+        while(temp){
+            mp[temp]++;
+            if(mp[temp] > 1)
+                return true;
+            temp = temp->next;
+        }
+        return false;
+    }
+};
+static const auto kds = []() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return 0;
+}();
