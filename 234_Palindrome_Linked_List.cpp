793efde0ff4c60fc5fa198f680bdee3d7313c12e
Yash Tekade <128163324+yashtekade21@users.noreply.github.com>
2024-12-11 13:57:37 +0530

    Create 234_Palindrome_Linked_List.cpp

---
+++ 234_Palindrome_Linked_List.cpp
@@ -0,0 +1,39 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    bool isPalindrome(ListNode* head) {
+        stack<int> st;
+
+        ListNode* node= head;
+        while(node){
+            st.push(node->val);
+            node = node->next;
+        }
+
+        node = head;
+        while(node){
+            if(node->val == st.top()){
+                st.pop();
+                node = node->next;
+            }
+            else
+                return false;
+        }
+        return true;
+    }
+};
+static const auto kds = []() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return 0;
+}();
