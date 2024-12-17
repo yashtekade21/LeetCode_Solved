e17b798ecd36eecedc05061bccbf6b32642d5f1c
Yash Tekade <128163324+yashtekade21@users.noreply.github.com>
2024-12-17 19:51:32 +0530

    Create 700_Search_in_a_Binary_Search_Tree.cpp

---
+++ 700_Search_in_a_Binary_Search_Tree.cpp
@@ -0,0 +1,32 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* searchBST(TreeNode* root, int val) {
+        TreeNode* node = root;
+        while(node){
+            if(node->val == val)
+                return node;
+            else if(node->val > val)
+                node = node->left;
+            else
+                node = node->right;
+        }
+        return node;
+    }
+};
+static const auto kds = []() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return 0;
+}();
