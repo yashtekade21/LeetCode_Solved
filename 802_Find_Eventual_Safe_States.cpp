2754b9bcf8251c50a3c5085c6a8487d31e00ebf1
Yash Tekade <128163324+yashtekade21@users.noreply.github.com>
2025-01-24 21:17:16 +0530

    Create 802_Find_Eventual_Safe_States.cpp

---
+++ 802_Find_Eventual_Safe_States.cpp
@@ -0,0 +1,54 @@
+class Solution {
+public:
+    vector<int> eventualSafeNodes(vector<vector<int>>& graph) {
+        int n = graph.size();
+        vector<vector<int>> new_graph(n);
+        vector<int> indegree(n, 0);
+        queue<int> q;
+        int count = 0;
+
+        for (int i = 0; i < n; i++) {
+            for (auto& j : graph[i]) {
+                new_graph[j].emplace_back(i);
+                indegree[i]++;
+            }
+        }
+
+        for (int i = 0; i < n; i++) {
+            if (indegree[i] == 0) {
+                q.push(i);
+                count++;
+            }
+        }
+
+        vector<bool> safe(n, false);
+
+        while (!q.empty()) {
+            int x = q.front();
+            q.pop();
+
+            safe[x] = true;
+
+            for (auto& e : new_graph[x]) {
+                indegree[e]--;
+
+                if (indegree[e] == 0) {
+                    q.push(e);
+                    count++;
+                }
+            }
+        }
+        vector<int> safenodes;
+        for (int i = 0; i < n; i++) {
+            if (safe[i])
+                safenodes.emplace_back(i);
+        }
+        return safenodes;
+    }
+};
+static const auto kds = []() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return 0;
+}();
