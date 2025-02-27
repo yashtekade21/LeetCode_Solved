cf793b8a3a9da9ede2f0cdcb87a31d0db6d187c6
Yash Tekade <128163324+yashtekade21@users.noreply.github.com>
2025-02-27 19:58:48 +0530

    Update 873_Length_of_Longest_Fibonacci_Subsequence.cpp

--- 873_Length_of_Longest_Fibonacci_Subsequence.cpp
+++ 873_Length_of_Longest_Fibonacci_Subsequence.cpp
@@ -3,6 +3,7 @@ public:
     int lenLongestFibSubseq(vector<int>& arr) {
         int n = arr.size();
         unordered_map<int,int> mp;
+        vector<vector<int>> dp(n,vector<int>(n,-1));
 
         for(int i=0;i<n;i++){
             mp[arr[i]] = i;
@@ -11,7 +12,7 @@ public:
         int maxSubSeqLen = 0;
         for(int k = n-1;k>=0;k--){
             for(int j = k-1;j>=0;j--){
-                int len = solve(j,k,arr,mp);
+                int len = solve(j,k,arr,mp,dp);
                 if(len >= 3)
                     maxSubSeqLen = max(maxSubSeqLen,len);
             }
@@ -20,14 +21,18 @@ public:
     }
 
 private:
-    int solve(int j,int k,vector<int>& arr,unordered_map<int,int>& mp){
+    int solve(int j,int k,vector<int>& arr,unordered_map<int,int>& mp,vector<vector<int>>& dp){
+
+        if(dp[j][k] != -1)
+            return dp[j][k];
+
         int nextFibElement = arr[k] - arr[j];
 
         if(mp.find(nextFibElement) != mp.end() && mp[nextFibElement] < j){
             int i = mp[nextFibElement];
-            return solve(i,j,arr,mp) + 1;  // 1 becoz to count the kth element
+            return dp[i][j] = solve(i,j,arr,mp,dp) + 1;  // 1 becoz to count the kth element
         }
-        return 2; // last 2 elements will not be counted that why return 2
+        return dp[j][k] = 2; // last 2 elements will not be counted that why return 2
     }
 };
 static const auto kds = []() {
