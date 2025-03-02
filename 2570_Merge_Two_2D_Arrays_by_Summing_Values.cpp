5798b2de906b334042ddcaa3173b8f5226fe5c7d
Yash Tekade <128163324+yashtekade21@users.noreply.github.com>
2025-03-02 09:10:20 +0530

    Update 2570_Merge_Two_2D_Arrays_by_Summing_Values.cpp

--- 2570_Merge_Two_2D_Arrays_by_Summing_Values.cpp
+++ 2570_Merge_Two_2D_Arrays_by_Summing_Values.cpp
@@ -31,3 +31,44 @@ static const auto kds = []() {
     cout.tie(nullptr);
     return 0;
 }();
+
+
+// class Solution {
+// public:
+//     vector<vector<int>> mergeArrays(vector<vector<int>>& nums1,
+//                                     vector<vector<int>>& nums2) {
+//         vector<vector<int>> result;
+//         int i = 0, j = 0;
+        
+//         while (i < nums1.size() && j < nums2.size()) {
+//             if (nums1[i][0] == nums2[j][0]) {
+//                 result.push_back({nums1[i][0], nums1[i][1] + nums2[j][1]});
+//                 i++;
+//                 j++;
+//             } else if (nums1[i][0] < nums2[j][0]) {
+//                 result.push_back(nums1[i]);
+//                 i++;
+//             } else {
+//                 result.push_back(nums2[j]);
+//                 j++;
+//             }
+//         }
+//         while (i < nums1.size()) {
+//             result.push_back(nums1[i]);
+//             i++;
+//         }
+
+//         while (j < nums2.size()) {
+//             result.push_back(nums2[j]);
+//             j++;
+//         }
+
+//         return result;
+//     }
+// };
+// static const auto kds = []() {
+//     ios::sync_with_stdio(false);
+//     cin.tie(nullptr);
+//     cout.tie(nullptr);
+//     return 0;
+// }();
