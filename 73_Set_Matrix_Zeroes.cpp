9aedb59efaced187788666e32dbfc4c62c684482
Yash Tekade <128163324+yashtekade21@users.noreply.github.com>
2025-05-21 09:07:38 +0530

    Update 73_Set_Matrix_Zeroes.cpp

--- 73_Set_Matrix_Zeroes.cpp
+++ 73_Set_Matrix_Zeroes.cpp
@@ -1,43 +1,33 @@
 class Solution {
 public:
     void setZeroes(vector<vector<int>>& matrix) {
-        int col0 = 1,n = matrix.size(),m = matrix[0].size();
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                if(matrix[i][j] == 0){
-                    matrix[i][0] = 0;
+        unordered_set<int> stRow, stCol;
+        int m = matrix.size(), n = matrix[0].size();
 
-                    if(j == 0)
-                        col0 = 0;
-                    else
-                        matrix[0][j] = 0;
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (matrix[i][j] == 0) {
+                    stRow.insert(i);
+                    stCol.insert(j);
                 }
             }
         }
 
-        for(int i=n-1;i>0;i--){
-            for(int j=m-1;j>0;j--){
-                if(matrix[i][j] != 0){
-                    if(matrix[0][j] == 0 || matrix[i][0] == 0)
-                        matrix[i][j] = 0;
-                }
-            }
+        for (auto& idx : stRow) {
+            for (int i = 0; i < n; i++)
+                matrix[idx][i] = 0;
         }
 
-        if(matrix[0][0] == 0)
-            for(int j=0;j<m;j++)
-                matrix[0][j] = 0;
-
-        if(col0==0)
-            for(int i=0;i<n;i++)
-                matrix[i][0] = 0;
-
+        for (auto& idx : stCol) {
+            for (int i = 0; i < m; i++)
+                matrix[i][idx] = 0;
+        }
         return;
     }
 };
 static const auto kds = []() {
-    std::ios::sync_with_stdio(false);
-    std::cin.tie(nullptr);
-    std::cout.tie(nullptr);
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
     return 0;
 }();
