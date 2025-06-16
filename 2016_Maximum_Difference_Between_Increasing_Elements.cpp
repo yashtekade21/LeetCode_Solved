f579334b0cde2419b8d013d1133c34e38acab465
Yash Tekade <128163324+yashtekade21@users.noreply.github.com>
2025-06-16 10:22:14 +0530

    Create 2016_Maximum_Difference_Between_Increasing_Elements.cpp

---
+++ 2016_Maximum_Difference_Between_Increasing_Elements.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int maximumDifference(vector<int>& nums) {
+        int ans = 0;
+        int n = nums.size();
+
+        int curMinIdx = INT_MAX,curMaxIdx = INT_MIN;
+        for(int i=0;i<n;i++){
+            if(curMinIdx == INT_MAX || nums[curMinIdx] > nums[i])
+                curMinIdx = i;
+            
+            ans = max(ans,abs(nums[i] - nums[curMinIdx]));
+        }
+        return (ans == 0) ? -1 : ans;
+    }
+};
+static const auto kds = []() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return 0;
+}();
