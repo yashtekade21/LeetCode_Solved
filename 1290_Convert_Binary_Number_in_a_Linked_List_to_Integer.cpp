06cbae3c2e066bf80ecda67bc21dd5e1ffd01512
Yash Tekade <128163324+yashtekade21@users.noreply.github.com>
2025-07-14 10:15:28 +0530

    Create 1290_Convert_Binary_Number_in_a_Linked_List_to_Integer.cpp

---
+++ 1290_Convert_Binary_Number_in_a_Linked_List_to_Integer.cpp
@@ -0,0 +1,32 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    int getDecimalValue(ListNode* head) {
+        ListNode* temp = head;
+
+        string binary = "";
+
+        while(temp != nullptr){
+            binary += to_string(temp->val);
+            temp = temp->next;
+        }
+
+        int ans = stoi(binary, nullptr, 2);
+        return ans;
+    }
+};
+static const auto kds = []() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return 0;
+}();
