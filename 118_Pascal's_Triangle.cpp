1fe656b97482d8ed0c6d2b8163b93d223472e1b2
Yash Tekade <128163324+yashtekade21@users.noreply.github.com>
2025-08-01 12:14:39 +0530

    Update 118_Pascal's_Triangle.cpp

--- 118_Pascal's_Triangle.cpp
+++ 118_Pascal's_Triangle.cpp
@@ -2,27 +2,30 @@ class Solution {
 public:
     vector<vector<int>> generate(int numRows) {
         vector<vector<int>> ans;
-        for(int i=1;i<=numRows;i++)
-            ans.emplace_back(generateRow(i));
+
+        for (int i = 1; i <= numRows; i++) 
+            ans.push_back(calcRow(i));
+
         return ans;
     }
-    private:
-    vector<int> generateRow(int row){
-    long long ans = 1;
-    vector<int> ansRow;
-    ansRow.emplace_back(ans);
-    for(int i=1;i<row;i++){
-        ans *= (row-i);
-        ans /= i;
 
-        ansRow.emplace_back(ans);
+private:
+    vector<int> calcRow(int rowNum) {
+        vector<int> rowVals;
+        int num = 1;
+
+        rowVals.push_back(num);
+        for (int i = 1; i < rowNum; i++) {
+            num *= (rowNum - i);
+            num /= i;
+            rowVals.push_back(num);
+        }
+        return rowVals;
     }
-    return ansRow;
-}
 };
 static const auto kds = []() {
-    std::ios::sync_with_stdio(false);
-    std::cin.tie(nullptr);
-    std::cout.tie(nullptr);
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
     return 0;
 }();
