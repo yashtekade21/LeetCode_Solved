74ad18a4fde996fbdd5507a37919a72fe0e29470
Yash Tekade <128163324+yashtekade21@users.noreply.github.com>
2025-09-05 22:41:01 +0530

    Create 2749_Minimum_Operations_to_Make_the_Integer_Zero.cpp

---
+++ 2749_Minimum_Operations_to_Make_the_Integer_Zero.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int makeTheIntegerZero(int num1, int num2) {
+        int t = 0;
+
+        while(true) {
+            long long val = (long long)num1 - (long long)t * num2;
+
+            if(val < 0)
+                return -1;
+            
+            if(__builtin_popcountll(val) <= t && t <= val) {
+                return t;
+            }
+
+            t++;
+        }
+
+        return -1;
+    }
+};
+static const auto kds = []() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return 0;
+}();
