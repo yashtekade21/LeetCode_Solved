20a07aa784935b07a57135721ac3c537824c2601
Yash Tekade <128163324+yashtekade21@users.noreply.github.com>
2025-11-09 10:53:30 +0530

    Create 2169_Count_Operations_to_Obtain_Zero.cpp

---
+++ 2169_Count_Operations_to_Obtain_Zero.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int countOperations(int num1, int num2) {
+        int ans = 0;
+        while (num1 != 0 && num2 != 0) {
+            if (num1 >= num2)
+                num1 -= num2;
+            else
+                num2 -= num1;
+            ans++;
+        }
+        return ans;
+    }
+};
+static const auto kds = []() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return 0;
+}();
