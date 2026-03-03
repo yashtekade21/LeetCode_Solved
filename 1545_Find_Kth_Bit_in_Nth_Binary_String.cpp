3456bfa3b89512bd876fe72acde4073d3d9d1ced
Yash Tekade <128163324+yashtekade21@users.noreply.github.com>
2026-03-03 21:51:35 +0530

    Update 1545_Find_Kth_Bit_in_Nth_Binary_String.cpp

--- 1545_Find_Kth_Bit_in_Nth_Binary_String.cpp
+++ 1545_Find_Kth_Bit_in_Nth_Binary_String.cpp
@@ -1,28 +1,19 @@
 class Solution {
 public:
     char findKthBit(int n, int k) {
-        int size = pow(2, n) - 1;
-        return find(size, k - 1);
-    }
-
-private:
-    char find(int size, int k) {
-        if (size == 1)
+        if (n == 1)
             return '0';
 
-        int mid = (size / 2);
+        int len = (1 << n) - 1; // pow(2,n) - 1
 
-        if (k == mid)
+        if (k < ceil(len / 2.0))
+            return findKthBit(n - 1, k);
+        else if (k == ceil(len / 2.0))
             return '1';
-        else if (k < mid)
-            return find(mid, k);
         else {
-            if (find(mid, size - k - 1) == '0')
-                return '1';
-            else
-                return '0';
+            char c = findKthBit(n - 1, len - (k - 1));
+            return c == '0' ? '1' : '0';
         }
-        return '0';
     }
 };
 static const auto kds = []() {
