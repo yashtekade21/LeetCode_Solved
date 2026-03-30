a4f954c3c2af72042a7c9bdb2a8e2795a62cb11e
Yash Tekade <128163324+yashtekade21@users.noreply.github.com>
2026-03-30 13:04:01 +0530

    Create 2840_Check_if_Strings_Can_be_Made_Equal_With_Operations_II.cpp

---
+++ 2840_Check_if_Strings_Can_be_Made_Equal_With_Operations_II.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    bool checkStrings(string s1, string s2) {
+        vector<int> even(26, 0), odd(26, 0);
+        int n = s1.length();
+
+        for (int i = 0; i < n; i++) {
+            if (i % 2 == 0) {
+                even[s1[i] - 'a']++;
+                even[s2[i] - 'a']--;
+            } else {
+                odd[s1[i] - 'a']++;
+                odd[s2[i] - 'a']--;
+            }
+        }
+
+        for (int i = 0; i < 26; i++) {
+            if (even[i] || odd[i])
+                return false;
+        }
+        return true;
+    }
+};
+static const auto kds = []() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return 0;
+}();
