72c9fd6c8c77dc11263000928c2fc25d66be46b7
Yash Tekade <128163324+yashtekade21@users.noreply.github.com>
2026-04-16 22:37:02 +0530

    Update 3488_Closest_Equal_Element_Queries.cpp

--- 3488_Closest_Equal_Element_Queries.cpp
+++ 3488_Closest_Equal_Element_Queries.cpp
@@ -1,25 +1,42 @@
 class Solution {
 public:
     vector<int> solveQueries(vector<int>& nums, vector<int>& queries) {
-        int n = nums.size();
-
-        nums.insert(nums.end(),nums.begin(),nums.end());
-        nums.insert(nums.end(),nums.begin(),nums.end());
-
-        set<pair<int, int>> st;
-        for (int i = 0; i < 3 * n; i++) 
-                st.insert({nums[i], i});
+        unordered_map<int, vector<int>> mp;
         vector<int> ans;
-        for (int q : queries) {
-            q += n;
-            auto it = st.lower_bound({nums[q], q});
-            ans.push_back(min({n, q - prev(it)->second, next(it)->second - q}));
-            if (ans.back() == n) ans.back() = -1;
+        int n = nums.size();
+        for (int i = 0; i < n; i++)
+            mp[nums[i]].emplace_back(i);
+
+        for (auto& query : queries) {
+            int num = nums[query];
+            vector<int>& vec = mp[num];
+            int vecSize = vec.size();
+
+            if (vec.size() == 1) {
+                ans.push_back(-1);
+                continue;
+            }
+
+            int idx = lower_bound(vec.begin(), vec.end(), query) - vec.begin();
+            int minD = INT_MAX;
+            int rightIdx = vec[(idx + 1) % vecSize];
+            int rd = abs(rightIdx - query);
+            int rcd = n - rd;
+
+            minD = min({minD, rd, rcd});
+
+            int leftIdx = vec[(idx - 1 + vecSize) % vecSize];
+            int ld = abs(leftIdx - query);
+            int lcd = n - ld;
+
+            minD = min({minD, ld, lcd});
+
+            ans.push_back(minD);
         }
+
         return ans;
     }
 };
-
 static const auto kds = []() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
