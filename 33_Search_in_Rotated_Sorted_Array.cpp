10a11337ea73099937517cbbe844cf392710310f
Yash Tekade <128163324+yashtekade21@users.noreply.github.com>
2026-05-22 18:52:16 +0530

    Update 33_Search_in_Rotated_Sorted_Array.cpp

--- 33_Search_in_Rotated_Sorted_Array.cpp
+++ 33_Search_in_Rotated_Sorted_Array.cpp
@@ -1,25 +1,33 @@
 class Solution {
 public:
+    int n;
     int search(vector<int>& nums, int target) {
-        int n = nums.size();
-        int low = 0, high = n - 1;
-        while (low <= high) {
-            int mid = (low + high) / 2;
-
-            if (nums[mid] == target)
-                return mid;
-
-            if (nums[low] <= nums[mid]) {
-                if (nums[low] <= target && nums[mid] >= target)
-                    high = mid - 1;
-                else
-                    low = mid + 1;
-            } else {
-                if (nums[mid] <= target && nums[high] >= target)
-                    low = mid + 1;
-                else
-                    high = mid - 1;
+        n = nums.size();
+        int l = 0, m, r = n - 1;
+        for (int i = 0; i < n - 1; i++) {
+            if (nums[i] > nums[i + 1]) {
+                m = i;
+                break;
             }
+        }
+
+        int ans1 = binSearch(nums, l, m, target),
+            ans2 = binSearch(nums, m + 1, r, target);
+        return (ans1 != -1) ? ans1 : (ans2 != -1) ? ans2 : -1;
+    }
+
+private:
+    int binSearch(vector<int>& nums, int l, int r, int target) {
+        while (l <= r) {
+            int m = (l + r) / 2;
+
+            if (nums[m] == target)
+                return m;
+
+            if (nums[m] > target)
+                r = m - 1;
+            else
+                l = m + 1;
         }
         return -1;
     }
