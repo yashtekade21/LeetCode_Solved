0edb488617e063c08a53df404e31640d32c6625d
Yash Tekade <128163324+yashtekade21@users.noreply.github.com>
2026-06-21 12:12:23 +0530

    Create 1833_Maximum_Ice_Cream_Bars.cpp

---
+++ 1833_Maximum_Ice_Cream_Bars.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int maxIceCream(vector<int>& costs, int coins) {
+        sort(costs.begin(), costs.end());
+        int ans = 0;
+        for (auto& cost : costs) {
+            if (coins < cost)
+                break;
+
+            coins -= cost;
+            ans++;
+        }
+        return ans;
+    }
+};
+static const auto kds = []() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return 0;
+}();
