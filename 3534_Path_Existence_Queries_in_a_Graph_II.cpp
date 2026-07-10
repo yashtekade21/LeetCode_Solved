cd052cfc6d85359999c7d102105e60c74f98b817
Yash Tekade <128163324+yashtekade21@users.noreply.github.com>
2026-07-10 22:56:34 +0530

    Create 3534_Path_Existence_Queries_in_a_Graph_II.cpp

---
+++ 3534_Path_Existence_Queries_in_a_Graph_II.cpp
@@ -0,0 +1,98 @@
+class Solution {
+public:
+    int rows;
+    int cols;
+    vector<vector<int>> ancestorTable;
+
+    vector<int> pathExistenceQueries(int n, vector<int>& nums, int maxDiff,
+                                     vector<vector<int>>& queries) {
+        vector<pair<int, int>> arr(n);
+        for (int i = 0; i < n; i++) {
+            arr[i] = {nums[i], i};
+        }
+
+        sort(begin(arr), end(arr));
+        vector<int> nodeToIdx(n);
+        for (int i = 0; i < n; i++) {
+            int node = arr[i].second;
+            nodeToIdx[node] = i;
+        }
+
+        rows = n;
+        cols = log2(n) + 1;
+        ancestorTable.resize(rows, vector<int>(cols, 0));
+
+        for (int node = 0; node < n; node++) {
+            int farthestIdxOneHop =
+                customUpperBound(arr, arr[node].first + maxDiff);
+            ancestorTable[node][0] = farthestIdxOneHop;
+        }
+
+        for (int j = 1; j < cols; j++) {
+            for (int node = 0; node < n; node++) {
+                ancestorTable[node][j] =
+                    ancestorTable[ancestorTable[node][j - 1]][j - 1];
+            }
+        }
+
+        vector<int> ans;
+        for (auto& query : queries) {
+            int u = query[0];
+            int v = query[1];
+
+            int a = nodeToIdx[u];
+            int b = nodeToIdx[v];
+            if (a == b) {
+                ans.push_back(0);
+                continue;
+            }
+
+            if (a > b)
+                swap(a, b);
+
+            int curr = a;
+            int jumps = 0;
+
+            for (int j = cols - 1; j >= 0; j--) {
+                if (ancestorTable[curr][j] < b) {
+                    curr = ancestorTable[curr][j];
+                    jumps += (1 << j);
+                }
+            }
+
+            if (ancestorTable[curr][0] >= b)
+                ans.push_back(jumps + 1);
+            else
+                ans.push_back(-1);
+        }
+
+        return ans;
+    }
+
+private:
+    int customUpperBound(vector<pair<int, int>>& arr, int target) {
+        int n = arr.size();
+        int l = 0;
+        int r = n - 1;
+        int res = 0;
+
+        while (l <= r) {
+            int mid = l + (r - l) / 2;
+
+            if (arr[mid].first <= target) {
+                res = mid;
+                l = mid + 1;
+            } else {
+                r = mid - 1;
+            }
+        }
+
+        return res;
+    }
+};
+static const auto kds = []() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return 0;
+}();
