2125fd4264a53e07044a0acdd58fc40d8ad7f5a7
Yash Tekade <128163324+yashtekade21@users.noreply.github.com>
2026-08-16 21:50:04 +0530

    Create 2029_Stone_Game_IX.cpp

---
+++ 2029_Stone_Game_IX.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool stoneGameIX(vector<int>& stones) {
+        int cnt[3] = {};
+        for (int a : stones)
+            cnt[a % 3]++;
+        if (min(cnt[1], cnt[2]) == 0)
+            return max(cnt[1], cnt[2]) > 2 && cnt[0] % 2 > 0;
+        return abs(cnt[1] - cnt[2]) > 2 || cnt[0] % 2 == 0;
+    }
+};
+static const auto kds = []() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return 0;
+}();
