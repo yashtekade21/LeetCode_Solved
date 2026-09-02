5e3fd797c80796890f7b156cd6cc257a3dabb9b1
Yash Tekade <128163324+yashtekade21@users.noreply.github.com>
2026-09-02 19:22:13 +0530

    Create 3875_Construct_Uniform_Parity_Array_I.cpp

---
+++ 3875_Construct_Uniform_Parity_Array_I.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    bool uniformArray(vector<int>& nums1) { return true; }
+};
+static const auto kds = []() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return 0;
+}();
